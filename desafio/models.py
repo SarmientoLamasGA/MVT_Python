8e71523e9e666514e66d4b5b9c34ce788eaf8a1d
Gabriel Alejandro Sarmiento Lamas <sarmiento_lamas.g.a@hotmail.com>
2022-11-14 21:06:51 -0300

    Desafío comenzado, creada DB

---
+++ desafio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Usuario(models.Model):
+    username = models.CharField(max_length=20, unique=True, null=False)
+    admin = models.BooleanField(null=False)
+    nombre = models.CharField(max_length=40, null=False)
+    apellido = models.CharField(max_length=40,null=False)
+    edad = models.IntegerField(null=False)
+
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=100,null=False)
+    autorNombre = models.CharField(max_length=40,null=False)
+    autorApellido = models.CharField(max_length=40,null=False)
+
+class Producto(models.Model):
+    nombre = models.CharField(max_length=50,null=False)
+    descripcion = models.CharField(max_length=250,null=False)
